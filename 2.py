b8a89d8c2f9ccb6130927e079a2e3dcb9d4af754
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-09-16 10:11:23 +0200

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,14 @@
+#en este apartado estamos definiento variable
+var1="xaloc"#este variable var1 es de tipo string y como puede observar esta letra 
+var2=4# esta variabble contienen un valor entero
+var3=5
+var4=1.8
+
+
+#este es un ejemplo de como no debe definir una variable var3="no es correcto
+print ("mi nombre es x.w")
+
+suma=var2+var3
+print (suma)
+print (var2+var3)
+print ("el resultado de la suma de var2+var3 es ",suma)
