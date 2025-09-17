8e7e6d1b3297f18cefb6fa3ea57bae064eda77e7
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-09-17 09:12:19 +0200

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,36 @@
+#en este apartado estamos definiento variable
+var1="xaloc"#este variable var1 es de tipo string y como puede observar esta letra 
+var2=4# esta variabble contienen un valor entero
+var3=5
+var4=1.8
+
+#este es un ejemplo de como no debe definir una variable var3="no es correcto
+print ("mi nombre es x.w")#ejemplo de la instruccion print que permite visual
+suma=var2+var3#he definido una nueva variable que contienen la suma
+print (suma)
+print (var2+var3)
+print ("el resultado de la suma de var2+var3 es ",suma)
+print ("error de introduccion)
+#A continuación hacemos uso de la funcion input, intruccion que permite captura
+#un valor introducido por el usuario via teclado.
+#recuerda que en la instrucción input podemos inreoducir un mensaje informativo
+#siempre entre comilla " "
+nombre=input("introduce tu nombre")
+apellido1=input("indruduce tu segundo apellido") 
+apellido2=input("introduce tu primero apellido")
+#he definido una nueva variable nombre_completo (recuerda que los variables no
+nombre_completo=nombre + " " + apellido1 + apellido
+#en este caso de la variables
+print(nombre_completo)
+suma=var2+var3
+print(f"la suma de (var2) + (var3) es igual a ", suma)
+resta=var2-var3
+print(f"la resta de (var2) - (var3) es igual a ", resta)
+multiplicacion=var2*var3
+print(f"la multiplicacion de (var2) * (var3) es igual a ", multiplicacion)
+division=var2/var3
+print(f"la division de (var2) / (var3) es igual a ", division)
+modulo_division=var2@var3
+print(f"el resto de division de (var2) / (var3) es igual a ", modulo_division)
+potencia=var2**var3)
+printf"la potencia de (var2) * (var3) es igual a ", potencia)
