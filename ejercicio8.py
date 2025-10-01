6ce09b5026b36f1455f0ec824903ac569567570c
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-01 09:45:53 +0200

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,9 +1,7 @@
 #programa8
-hora1=input("indroduce la primera hora ")
-segundos1=10800
-print("Número de segundos en 180 minutos",segundos1)
-hora2=input("indroduce la segundo hora ")
-segundos2=3600
-print("Número de segundos en 180 minutos",segundos2)
+hora1=input("indroduce la  hora ")
+print("Número de minutos es",hora1*60)
+print("Número de segundos es",hora1*3600)
+
 
 
