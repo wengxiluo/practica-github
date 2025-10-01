975726746f5365c49bacfe9901e6751b6a5d992f
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-01 09:10:18 +0200

    Update ejercicio2py.py

--- ejercicio2py.py
+++ ejercicio2py.py
@@ -1,14 +1,15 @@
 #programa1.1
-var1 = 1
-var2="A"
-var3=6.8
-var4=3.4
-var5="B"
-var6=5.4
+var1 = input("introduce numero")
+var2=input("introduce letra)
+var3=input("introduce numero")
+var4=input("introduce numero")
+var5=input("introduce letra")
+var6=input("introduce numero")
 print(var1)
 print(var2)
 print(var3)
 print(var4)
 print(var5)
 print(var6)
+
 
