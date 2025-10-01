ee7c5d9073b3950b584b49044414db5702d4ec59
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-01 09:24:03 +0200

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,12 +1,15 @@
 #programa3
 #este es numero de VALOR
-var1=3
-var2=4
-var3=6
-var4=6
-suma=var1+var2#codigo de suma
-print(suma)
-print(var3+var4)
+var1=input("introduce el primer numero")
+var2=input("introduce el segundo numero")
+resultado=int(var1)+(var2)
+print(f"la suma de (var1)+(var2)es igual a ",resultado)
+var3=input("introduce el tercero numero")
+var4=input("introduce el cuarto numero")
+resultado1=int(var3)+(var4)
+print(f"la suma de (var3)+(var4)es igual a ",resultado1)
+
+
 
 
 
