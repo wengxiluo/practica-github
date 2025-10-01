26b2d3f88d084eecb54fdfdc868a3c84bd7d17e8
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-01 09:25:56 +0200

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,10 +4,8 @@ var1=input("introduce el primer numero")
 var2=input("introduce el segundo numero")
 resultado=int(var1)+(var2)
 print(f"la suma de (var1)+(var2)es igual a ",resultado)
-var3=input("introduce el tercero numero")
-var4=input("introduce el cuarto numero")
-resultado1=int(var3)+(var4)
-print(f"la suma de (var3)+(var4)es igual a ",resultado1)
+
+
 
 
 
