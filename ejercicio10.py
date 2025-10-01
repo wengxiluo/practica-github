493663e59bb1860cea6b68ce60c6c85df5b01e10
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-01 09:53:38 +0200

    Update and rename ejercicio9 to ejercicio10.py

---
+++ ejercicio10.py
@@ -0,0 +1,7 @@
+var1=int(input("introduce el primer numero"))
+var2=int(input("introduce el segundo numero"))
+print("el cociente de les dos numeros" ,var1/var2)
+if(var1%2==0)
+  :print("el var de par")
+else
+  :print("el var de impar
