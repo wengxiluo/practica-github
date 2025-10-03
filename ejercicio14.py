2641ccfea800f4d745b222cbcc371f8548270f97
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-03 16:03:48 +0200

    Create ejercicio14.py

---
+++ ejercicio14.py
@@ -0,0 +1,8 @@
+#programa14
+import math
+diametro=input("introduce el diametro de un circulo")
+perímetro=float(diametro)*math.
+radio=float(diametro)/(2*math.pi)
+área=(radio*math.pi)**2
+print(f"el perimetro del circulo és:{perímetro:.1f}")
+print(f"el área del circulo és:{área:.1f}")
