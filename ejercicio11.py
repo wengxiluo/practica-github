3b49a9e1b6bd48cceaa79eaa2fcc887af4809eae
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-03 15:31:30 +0200

    Update and rename ejercicios 11 to ejercicio11.py

---
+++ ejercicio11.py
@@ -0,0 +1,6 @@
+#programa
+lado=int(input("introduce el lado de el cuadrado")
+perimetro=int(lado)*4
+area=int(lado)*int(lado)
+print("el perimetro del cuadrado es", perimetro)
+print("el area del cuadrado es",area)
