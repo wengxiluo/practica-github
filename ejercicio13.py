4c4518094efc6cde18aeaadd4884f30e1e51d3bd
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-03 15:47:17 +0200

    Create ejercicio13.py

---
+++ ejercicio13.py
@@ -0,0 +1,6 @@
+#programa13
+lado=input("introduce el valor del lado de un cubo")
+el área=float(lado)*float(lado)*6
+volumen=float(lado)**3
+print(f"el área del cubo és:{área}")
+print(f"el volumen del cubo és:{volumen}")
