b3408c689e787f25da9b5a56950d157f91c8cf57
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-07 10:36:08 +0200

    Create ejercicio.16.py

---
+++ ejercicio.16.py
@@ -0,0 +1,7 @@
+#programa16
+import math
+var1=input("introduce un numero")
+raiz=math.sqrt(int(var1))
+divicion=int(raiz/2)
+print(f"El resutaldo de la raiz es:{raiz:.if}")
+print(f"resultado de la division:{division}")
