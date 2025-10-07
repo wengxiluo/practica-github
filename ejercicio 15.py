778f1b54e7ecc5fa570d568803ca96ce313fb8f7
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-07 10:21:02 +0200

    Create ejercicio 15.py

---
+++ ejercicio 15.py
@@ -0,0 +1,7 @@
+#programa15
+import math
+radio=input("introduce el valor de radio de un cllindre")
+altura=float(radio)*float(radio)*math.pi*2+float(altura)*float(radio)*math.pi
+volumen=float(radio)*float(radio)*math.pi*float(altura)
+print(f"El área del cllindre és:{area:.2f}")
+print(f"El volumen del cllindre és:{volumen:.2f}")
