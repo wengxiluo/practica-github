92742116903f18529e5fdc54e11f91754c10fbe1
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-08 09:42:29 +0200

    Create ejercicio.18.py

---
+++ ejercicio.18.py
@@ -0,0 +1,8 @@
+#programa18
+menores=input("introduce número de menores que asistes al cine")
+adultos=input("introduce número de adultos que asistes al cine")
+descuento_50=float(menores)*12/2
+descuento_10=float(adultos)*12*0.9
+descuento_10=(float(adultos)*12)-(float(adultos)*12.0.1)
+print(f"el precio total del cine para {menores} menor/es és:{descuento_50}")
+print(f"el precio total del cine para {adultos} adulto/s és:{descuento_10}")
