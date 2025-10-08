c1d490698ddc5c7ebff2068e3e6bc4f5568309a5
xiluo.w <xiluo.weng@Campus.xaloc.org>
2025-10-08 09:31:01 +0200

    Update ejercicio.17.py

--- ejercicio.17.py
+++ ejercicio.17.py
@@ -1,1 +1,8 @@
 #programa17
+kilos=input("introduce el valor de tu peso")
+altura=input("introduce el valor de tu altura")
+imc=float(kilos)/float(float(altura)**2)
+peso=(" ")
+if (imc) >25 or (imc)==25
+pesos=("hay sobrepeso")
+print(f"si pesas {kilos} kilos y mides {altura}, tu IMC es :{inc:.2f} . ",peso)
